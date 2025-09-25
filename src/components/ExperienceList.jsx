5a6d5f724a062116b185158ae65b173de151377a
Elizaveta Novitskaya <eliza.novizkaya@gmail.com>
2025-09-25 20:01:32 +0300

    Реализованы соты на большие размеры экрана

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -46,7 +46,7 @@ const products = [
 ]
 
 export default function ExperienceList() {
-  // const [isShowText, setIsShowText] = useState(false);
+  const [isShowText, setIsShowText] = useState(false);
 
   return (
     <div className="bg-white">
@@ -55,35 +55,63 @@ export default function ExperienceList() {
           В нашей экспедиции
         </h2>
 
-        <div className="grid grid-cols-1 mt-24">
+        <div className="grid grid-cols-1 mt-24 lg:grid-cols-3 gap-0">
           {products.map((product) => (
             <div key={product.id}
-               // onMouseEnter={() => setIsShowText(product.id)}
-               // onMouseLeave={() => setIsShowText(false)}
+               onMouseEnter={() => setIsShowText(product.id)}
+               onMouseLeave={() => setIsShowText(false)}
                className={`
                 flex justify-between relative ${product.id % 2 === 0 ? 'flex-row-reverse' : ''}
             `}>
-              <div className={`flex flex-1/3 justify-center items-center ${product.id % 2 === 0 ? 'pl-4' : 'pr-4'}  `}>
+              <div className={`flex flex-1/3 justify-center items-center ${product.id % 2 === 0 ? 'pl-4' : 'pr-4'} lg:hidden`}>
                 <p
-                className={`mt-1 text-sm sm:text-xl text-gray-500 ${product.id % 2 === 0 ? 'text-right' : ''}`}>
+                className={`text-sm sm:text-xl text-gray-500 ${product.id % 2 === 0 ? 'text-right' : ''}`}>
                 {product.description}
                 </p>
               </div>
-              <div className="relative flex-2/3">
+              <div className={`
+                relative flex-2/3 p-0 m-0
+                ${product.id === 1 || product.id === 4 
+                ? 'top-1/2 left-1/4' : ''} 
+                ${product.id === 3 || product.id === 6
+                ? 'top-1/2 right-1/4' : ''} 
+                ${product.id === 7 
+                ? 'left-2/2' : ''}
+                `}>
                 <div
-                  className="w-full h-full overflow-hidden p-1 sm:p-2 bg-cyan-700"
+                  className={`w-full h-full overflow-hidden p-1 sm:p-2 bg-cyan-700 m-0 z-10 flex items-center justify-center
+                  `}
                   style={{
-                    clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)"
+                    clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
                   }}
                 >
                   <img
                     alt={product.imageAlt}
                     src={product.imageSrc}
-                    className="object-cover w-full h-full"
+                    className={`
+                      object-cover w-full h-full z-10
+                      lg:${isShowText === product.id ? 'hidden' : 'block' }
+                    `}
                     style={{
                       clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)"
                     }}
                   />
+                  <div
+                    className={`
+                      bg-white w-[28vw] h-[24vw] hidden p-12
+                      lg:${isShowText === product.id ? 'flex items-center justify-center ' : '' }
+                    `}
+                    style={{
+                      clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)"
+                    }}
+                  >
+                    <p
+                      className={`
+                        text-xl text-gray-500 text-center m-0 p-0
+                      `}>
+                      {product.description}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
